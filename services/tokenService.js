56e11c9ae06b5907087641995402fe2e3a1ea319
ANDNAZ <nazaryan-ando@mail.ru>
2023-03-02 01:56:32 +0400

    normalize model

--- services/tokenService.js
+++ services/tokenService.js
@@ -5,19 +5,16 @@ class TokenService {
 	generateTokens(payload) {
 		const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: process.env.JWT_ACCESS_EXPIRES_IN })
 		const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: process.env.JWT_REFRESH_EXPIRES_IN })
-		return {
-			accessToken,
-			refreshToken
-		}
+		return { accessToken, refreshToken }
 	}
 
 	async saveToken(userId, refreshToken) {
-		const tokenData = await Token.findOne({ where: { UserId: userId } })
+		const tokenData = await Token.findOne({ where: { userId } })
 		if (tokenData) {
 			await Token.update({ refreshToken }, { where: { id: tokenData.dataValues.id } });
 			return refreshToken
 		} else {
-			await Token.create({ UserId: userId, refreshToken })
+			await Token.create({ userId, refreshToken })
 			return refreshToken
 		}
 	}
